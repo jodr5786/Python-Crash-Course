36e11fc09d826fbb73d199185cd041b8d86de47b
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-01-25 13:30:35 -0500

    changed file name; edited

---
+++ Chapter 5/5-1_Conditional_Tests.py
@@ -0,0 +1,15 @@
+car = 'subaru' 
+print("Is car == 'subaru'? I predict True.") 
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+number = 99
+print("\nThe number is equal to " + str(number))
+if number < 21:
+    print("\nNumber is less than 21; true.")
+elif number >= 21 and number <= 50:
+    print("\nNumber is between 21 and 50.")
+elif number >= 51 and number <= 100:
+    print("\nThe number is between 51 and 100.")
