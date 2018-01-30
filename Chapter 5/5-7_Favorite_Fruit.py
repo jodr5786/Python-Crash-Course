5123f49543a4fda800e6a1252569b856716d02c9
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-01-30 14:04:25 -0500

    edits

---
+++ Chapter 5/5-7_Favorite_Fruit.py
@@ -0,0 +1,13 @@
+favorite_fruits = ['strawberries','blueberries','peaches']
+
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+if 'peaches' in favorite_fruits:
+    print("You really like peaches!")
+if 'pineapples' in favorite_fruits:
+    print("You really like pineapples!")
+if 'pineapples' in favorite_fruits:
+    print("You really like pineapples!")
+    
