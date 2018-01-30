56178dbf8f4384deb306c73d5a9c350583af28f3
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-01-30 11:16:40 -0500

    edits

---
+++ Chapter 5/5-4_Alien_Colors_#2.py
@@ -0,0 +1,7 @@
+alien_color = 'blue'
+
+if alien_color == 'green':
+    points = 5
+elif alien_color != 'green':
+    points = 10
+print("You earned " + str(points) + " points!")
---
+++ Chapter 5/5-5_Alien_Colors_#3.py
@@ -0,0 +1,9 @@
+alien_color = 'red'
+
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'yellow':
+    points = 10
+elif alien_color == 'red':
+    points = 15
+print("You earned " + str(points) + " points!")
