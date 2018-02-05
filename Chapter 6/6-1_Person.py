0bad48935cdfb914f311edea837ac1add010033a
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-02-05 12:28:43 -0500

    new files

---
+++ Chapter 6/6-1_Person.py
@@ -0,0 +1,11 @@
+person_info = {
+    'first_name': 'deanna',
+    'last_name': 'rawlins',
+    'age': 34,
+    'city': 'mount dora',
+}
+
+print("First name is " + person_info['first_name'].title() + ".")
+print("Last name is " + person_info['last_name'].title() + ".")
+print("Age is " + str(person_info['age']) + ".")
+print("City is " + person_info['city'].title() + ".")
