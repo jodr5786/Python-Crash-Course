963fcf1b18cf6410bf71a37b47aa25364a3ad230
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-02-05 14:11:10 -0500

    edits

---
+++ Chapter 6/6-2_Favorite_Numbers.py
@@ -0,0 +1,13 @@
+numbers = {
+    'sam': 5,
+    'larry': 22,
+    'greg': 13,
+    'elen': 12,
+    'tim': 54,
+}
+
+print("Sam's favorite number is " + str(numbers['sam']) + ".")
+print("Larry's favorite number is " + str(numbers['larry']) + ".")
+print("Greg's favorite number is " + str(numbers['greg']) + ".")
+print("Elen's favorite number is " + str(numbers['elen']) + ".")
+print("Tim's favorite number is " + str(numbers['tim']) + ".")
---
+++ Chapter 6/6-3_Glossary.py
@@ -0,0 +1,27 @@
+glossary = {
+    'string': 'a word',
+    'variable': 'a value',
+    'loop': 'repeated action on something',
+    'dictionary': 'a key-value pair',
+    'array': 'a list of things'
+}
+
+word = 'string'
+print("String: ")
+print("    " + glossary[word])
+
+word = 'variable'
+print("\nVariable: ")
+print("    " + glossary[word])
+
+word = 'loop'
+print("\nLoop: ")
+print("    " + glossary[word])
+
+word = 'dictionary'
+print("\nDictionary: ")
+print("    " + glossary[word])
+
+word = 'array'
+print("\nArray: ")
+print("    " + glossary[word])
