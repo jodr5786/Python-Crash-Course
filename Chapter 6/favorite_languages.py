8e4ea6f4bf8182d38ca3ac7906ea54c0a1340bf6
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-02-09 15:26:03 -0500

    edits

--- Chapter 6/favorite_languages.py
+++ Chapter 6/favorite_languages.py
@@ -6,6 +6,6 @@ favorite_languages = {
 }
 
 for name, languages in favorite_languages.items():
-    print("\n" + name.title() + "'s favorite are:")
+    print("\n" + name.title() + "'s favorite languages are:")
     for language in languages:
         print("\t" + language.title())
