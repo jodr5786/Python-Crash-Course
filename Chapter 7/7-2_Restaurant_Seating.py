20a8bde82b9a470730e862aca46fa73d11643e3c
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-02-12 13:11:42 -0500

    new files

---
+++ Chapter 7/7-2_Restaurant_Seating.py
@@ -0,0 +1,8 @@
+number_in_group = input("How many people are in your group? ")
+
+number_in_group = int(number_in_group)
+
+if number_in_group > 8:
+    print("\nI'm sorry, you'll have to wait for a table.")
+else:
+    print("\nFollow me, your table is this way.")
