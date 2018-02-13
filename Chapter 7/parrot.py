11253d6111fddb34a3045f71a1d0495f72df7285
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-02-13 07:50:43 -0500

    added if statement

--- Chapter 7/parrot.py
+++ Chapter 7/parrot.py
@@ -3,4 +3,6 @@ prompt += "\nEnter 'quit' to end the program. "
 message = ""
 while message != 'quit':
     message = input(prompt)
-    print(message)
+    
+    if message != 'quit':
+        print(message)
