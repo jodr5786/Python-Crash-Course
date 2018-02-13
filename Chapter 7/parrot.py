9e34f742485b665133275c78c5cb08209c9bf4f7
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-02-13 07:48:23 -0500

    fixed indent

--- Chapter 7/parrot.py
+++ Chapter 7/parrot.py
@@ -1,5 +1,5 @@
 prompt = "\nTell me something, and I will repeat it back to you:"
- prompt += "\nEnter 'quit' to end the program. "
+prompt += "\nEnter 'quit' to end the program. "
 message = ""
 while message != 'quit':
     message = input(prompt)
