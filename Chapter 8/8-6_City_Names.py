a9012871ae6df2cfb3f099378d7be072276a241e
Josh Rawlins <joshdrawlins@gmail.com>
2018-02-19 20:40:31 -0500

    edits

---
+++ Chapter 8/8-6_City_Names.py
@@ -0,0 +1,12 @@
+def city_country(city, country):
+    """Prints a city and country pair."""
+    return(city.title() + ", " + country.title())
+
+city = city_country('st. louis', 'united states')
+print(city)
+
+city = city_country('asheville', 'united states')
+print(city)
+
+city = city_country('orlando', 'united states')
+print(city)
