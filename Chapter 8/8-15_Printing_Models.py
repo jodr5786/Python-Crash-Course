dc63166e0f154e50cc2ba49b2e7d49ce50462e77
Josh Rawlins <joshdrawlins@gmail.com>
2018-03-05 20:45:05 -0500

    edits

---
+++ Chapter 8/8-15_Printing_Models.py
@@ -0,0 +1,10 @@
+from printing_functions import print_models
+from printing_functions import show_completed_models
+
+
+unprinted_designs = ['iphone case', 'robot pendant',
+'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
